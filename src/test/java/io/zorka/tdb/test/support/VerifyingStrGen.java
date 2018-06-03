6d643d24249293ca6ac85c970ad683ea39191e4a
Rafal Lewczuk <rafal.lewczuk@jitlogic.com>
2018-06-03 12:54:54 +0200

    Update copyright notices.

--- src/test/java/io/zorka/tdb/test/support/VerifyingStrGen.java
+++ src/test/java/io/zorka/tdb/test/support/VerifyingStrGen.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2017 Rafal Lewczuk <rafal.lewczuk@jitlogic.com>
+ * Copyright 2016-2018 Rafal Lewczuk <rafal.lewczuk@jitlogic.com>
  * <p/>
  * This is free software. You can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
